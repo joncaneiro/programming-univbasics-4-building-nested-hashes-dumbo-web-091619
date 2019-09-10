8dfee4300ce6e0a8b1746fe7fda22dbfaff1e080
Jon Caneiro <joncaneiro@gmail.com>
2019-09-10 22:50:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,10 +2,8 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
   
-	monopoly = {
-	  :hash => {
-	    :railraods => {}}
-	
+  hash = {:key => "value"}
+  
 end
 
 
