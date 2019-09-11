a91c4acb455e284fd00ea892f06d2dca1dabd862
Jon Caneiro <joncaneiro@gmail.com>
2019-09-11 01:12:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,14 +54,17 @@ def monopoly_with_fourth_tier
         names: {
           reading_railroad: {
            "mortgage_value" => "$100"
-            
           },
           pennsylvania_railroad: {
            "mortgage_value" => "$200" 
-            
           },
-          b_and_o_railroad: {},
-          shortline_railroad: {}
+          b_and_o_railroad: {
+            "mortgage_value" => "400"
+          },
+          shortline_railroad: {
+            "mortgage_value" => "$100"
+          }
+          
         },
       
         rent_in_dollars: {
