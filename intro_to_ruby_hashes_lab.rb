e861c02b375b451182f56e9ba48b208ec6df7595
Jon Caneiro <joncaneiro@gmail.com>
2019-09-11 00:57:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,9 +36,9 @@ def monopoly_with_third_tier
          
         names: {
           reading_railroad: {},
-          key2: [],
-          key3: [],
-          key4: []
+          pennsylvania_railroad: {},
+          key3: {},
+          key4: {}
         },
       
         rent_in_dollars: {
