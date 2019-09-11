bbd935a7240bb79881d1542a0b60b7584f374b0c
Jon Caneiro <joncaneiro@gmail.com>
2019-09-11 00:49:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,10 +34,10 @@ def monopoly_with_third_tier
   hash = {
     :railroads => {
         rent_in_dollars: {
-          key1: ,
-          key2: ,
-          key3: ,
-          key4: 
+          key1: [],
+          key2: [],
+          key3: [],
+          key4: []
         },
         pieces: 4,
      }
